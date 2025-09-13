ffac451d41af1b7c63fd3a8cc57e9d374c3d287d
maha705 <maha20061976@gmail.com>
2025-09-13 22:00:51 +0530

    Create server.js

---
+++ client/server/server.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors());
+app.use(express.static('../client'));
+
+// Store connected users
+const users = {};
+const classrooms = {};
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  // Join classroom
+  socket.on('join-classroom', (data) => {
+    const { classroomId, userType, userName } = data;
+    
+    socket.join(classroomId);
+    users[socket.id] = { classroomId, userType, userName };
+    
+    if (!classrooms[classroomId]) {
+      classrooms[classroomId] = { teacher: null, students: [] };
+    }
+    
+    if (userType === 'teacher') {
+      classrooms[classroomId].teacher = socket.id;
+    } else {
+      classrooms[classroomId].students.push(socket.id);
+    }
+    
+    // Notify others in classroom
+    socket.to(classroomId).emit('user-joined', {
+      userId: socket.id,
+      userType,
+      userName
+    });
+    
+    // Send current classroom state to new user
+    socket.emit('classroom-state', classrooms[classroomId]);
+  });
+
+  // WebRTC signaling
+  socket.on('offer', (data) => {
+    socket.to(data.target).emit('offer', {
+      offer: data.offer,
+      sender: socket.id
+    });
+  });
+
+  socket.on('answer', (data) => {
+    socket.to(data.target).emit('answer', {
+      answer: data.answer,
+      sender: socket.id
+    });
+  });
+
+  socket.on('ice-candidate', (data) => {
+    socket.to(data.target).emit('ice-candidate', {
+      candidate: data.candidate,
+      sender: socket.id
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+    
+    if (users[socket.id]) {
+      const { classroomId } = users[socket.id];
+      socket.to(classroomId).emit('user-left', socket.id);
+      
+      // Clean up classroom data
+      if (classrooms[classroomId]) {
+        if (classrooms[classroomId].teacher === socket.id) {
+          classrooms[classroomId].teacher = null;
+        } else {
+          classrooms[classroomId].students = 
+            classrooms[classroomId].students.filter(id => id !== socket.id);
+        }
+      }
+    }
+    
+    delete users[socket.id];
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`Open http://localhost:${PORT} in your browser`);
+});
